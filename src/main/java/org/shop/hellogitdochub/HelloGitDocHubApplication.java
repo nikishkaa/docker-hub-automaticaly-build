aaef5a8555f1a6fee1166345ac42d8bac1f4626e
nikishkaa <nikita.goutor@gmail.com>
2025-06-06 19:51:11 +0300

    Act.

--- src/main/java/org/shop/hellogitdochub/HelloGitDocHubApplication.java
+++ src/main/java/org/shop/hellogitdochub/HelloGitDocHubApplication.java
@@ -9,6 +9,7 @@ public class HelloGitDocHubApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloGitDocHubApplication.class, args);
 
+
         System.out.println("Hello World");
 
 
